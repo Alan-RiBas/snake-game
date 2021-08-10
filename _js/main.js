a07f437599533eb211a6e8a2a34e5433150f016e
Alan-RiBas <alanbatista.ribas@gmail.com>
2021-08-09 23:38:51 -0300

    criar o campo e a cobrinha

---
+++ _js/main.js
@@ -0,0 +1,53 @@
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+/*
+var idade = prompt('Qual a sua idade?')
+if(idade >= 18){
+    alert('Maior de idade');
+} else{
+    alert('Menor de idade');
+}
+*/
+/*
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome:"uva", cor: "roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    console.log(count);
+    alert(count);
+}
+*/
+
+/*
+var d = new Date();
+alert(d.getMonth());
+alert(d.getDay()+1);
+alert(d.getYear());
+alert(d.getHours());
+alert(d.getMinutes());
+alert(d.getSeconds());
+*/
+
+/*
+var lista = [“Alemanha”, “Inglaterra”, “Escócia”]/
+lista.push(“Polônia”);
+lista.pop(“Inglaterra”);
+console.log(lista.lenght[]);*/
+
+//console.log("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
+
+let line = gets().split(" ");
+let A = parseInt(line[11]);
+let B = parseInt(line[1]);
+let total = A + B; // Altere o valor da variável com o cálculo esperado
+console.log("X = " + total);
